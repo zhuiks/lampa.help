a4f1eb804e6dae833a927fa3e2990c67c302d87a
zhuiks <zhuke@ukr.net>
2021-06-23 17:30:15 -0500

    add basic folder structure

---
+++ src/routes/plan/lesson[num].svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from '$app/stores';
+  import Lesson from '$lib/lesson.svelte';
+
+  const { num } = $page.params;  
+</script>
+
+
+<h1>Lesson #{num}</h1>
+<Lesson {num}/>
