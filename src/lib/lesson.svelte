019d4cdc92dad18d0cc9f4659f67d0d0b3f5e9e1
zhuiks <zhuke@ukr.net>
2021-06-24 18:03:49 -0500

    adjust Lesson layout

--- src/lib/lesson.svelte
+++ src/lib/lesson.svelte
@@ -1,61 +1,78 @@
 <script>
-  import 'wired-elements/lib/wired-card.js';
-  import 'wired-elements/lib/wired-divider.js';
-  import LessonHeader from './lesson-header.svelte';
-  import MemoryVerse from './memory-verse.svelte';
+	import 'wired-elements/lib/wired-card.js';
+	import 'wired-elements/lib/wired-divider.js';
+	import LessonHeader from './lesson-header.svelte';
+	import MemoryVerse from './memory-verse.svelte';
 
-  export let section;
-  export let lesson;
+	export let section;
+	export let lesson;
 
-  function sunitize(str) {
-    return str.replace(/</g, '').replace(/>/g, '').replace('\n', '<br/>');
-  }
+	function sunitize(str) {
+		return str.replace(/</g, '').replace(/>/g, '').replace('\n', '<br/>');
+	}
 </script>
 
 <article class="lesson">
-  <LessonHeader
-    lessonNumber={lesson['Number']}
-    lessonTitle={lesson['Title']}
-    lessonSection={section}
-    lessonScripture={lesson['Scripture']} />
+	<LessonHeader
+		lessonNumber={lesson['Number']}
+		lessonTitle={lesson['Title']}
+		lessonSection={section}
+		lessonScripture={lesson['Scripture']}
+	/>
 
-  <MemoryVerse verse={lesson['Verse']} />  
+	<MemoryVerse verse={lesson['Verse']} />
 
-  <h3>Main Truth</h3>
-  <h2>{lesson['Truth']}</h2>
+	<h3>Main Truth</h3>
+	<h2>{lesson['Truth']}</h2>
 
-  <section>
-    <h3>Summary</h3>
-    <p>{@html sunitize(lesson['Summary'])}</p>
+	<section>
+		<h3>Summary</h3>
+		<p>{@html sunitize(lesson['Summary'])}</p>
+	</section>
 
-    <wired-card elevation="2">
-      <h3>God's character</h3>
-      <p>{lesson['Character']}</p>
-    </wired-card>
-  </section>
+	<section class="character">
+		<wired-card elevation="2">
+			<h3>God's character</h3>
+			<p>{lesson['Character']}</p>
+		</wired-card>
+	</section>
 
-  <section>
-    <h3>Application</h3>
-    <p>{@html sunitize(lesson['Application'])}</p>
-  </section>
+	<section>
+		<h3>Application</h3>
+		<p>{@html sunitize(lesson['Application'])}</p>
+	</section>
 
-  <footer>
-    <h3>Song suggestions</h3>
-    <ul>
-    {#each lesson['Songs'] as song}
-      <li>{song}</li>
-    {/each}
-    </ul>
-  </footer>
+	<footer>
+		<h3>Song suggestions</h3>
+		<ul>
+			{#each lesson['Songs'] as song}
+				<li>{song}</li>
+			{/each}
+		</ul>
+	</footer>
 </article>
 
 <style>
-  .lesson {
-    padding: 1em 2em;
-    clear: both;
-  }
+	.lesson {
+		padding: 1em 2em;
+		clear: both;
+	}
 
-  h2 {
-    color: var(--orange);
+	h2 {
+		color: var(--orange);
+	}
+
+	.character {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .character > wired-card {
+		max-width: 80%;
+    padding: 1em;
+	}
+  .character h3 {
+    margin-top: 0.45em;
+    margin-bottom: 0.45em;
   }
 </style>
