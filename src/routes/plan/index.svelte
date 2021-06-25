74600084e569cae4b9febcc064ee2619af8ca593
zhuiks <zhuke@ukr.net>
2021-06-25 12:01:43 -0500

    refactor lesson naming

--- src/routes/plan/index.svelte
+++ src/routes/plan/index.svelte
@@ -1,58 +1,67 @@
 <script context="module">
-  import { getYamlData, padNumber } from '$lib/get-data.js';
+	import { getYamlData, padNumber } from '$lib/get-data.js';
 
 	/**
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ page, fetch, session, context }) {
-		const prefix = "lessons/";
+		const prefix = 'lessons/';
 		const resAbout = await getYamlData(page, `${prefix}about`);
 
-    if (!resAbout.ok) {
+		if (!resAbout.ok) {
 			return resAbout;
 		}
 
-    const sections = await Promise.all(
-      resAbout.data['Sections'].map(async sectionData => {
-        const [sectionKey, list] = Object.entries(sectionData)[0];
-        let lessons = [];
+		const sections = await Promise.all(
+			resAbout.data['Sections'].map(async (sectionData) => {
+				const [sectionKey, details] = Object.entries(sectionData)[0];
+				let lessons = [];
 
-        if (Array.isArray(list)) {
-          for(let i=0; i<list.length; i++) {
-            const res = await getYamlData(page, `${prefix}${list[i]}`);
+				if (Array.isArray(details)) {
+					for (let i = 0; i < details.length; i++) {
+						const res = await getYamlData(page, `${prefix}${details[i]}`);
 
-            if (res.ok) {
-              lessons.push({
-                title: res.data['Title'],
-                link: `plan/${list[i]}`,
-              });
-            }
-          } 
-        } else if (Number.isInteger(list)) {
-          for(let i=1; i<=list; i++) {
-            const res = await getYamlData(page, `${prefix}lesson-${padNumber(i)}`);
+						if (res.ok) {
+							lessons.push({
+								title: res.data['Title'],
+								link: `plan/${details[i]}`
+							});
+						}
+					}
+				} else {
+					const totalLessons = details['Lessons'];
+					
+          if (Number.isInteger(totalLessons)) {
+						for (let i = 1; i <= totalLessons; i++) {
+							const res = await getYamlData(
+								page,
+								`${prefix}${sectionKey.toLowerCase().replace('_', '-')}-${padNumber(i)}`
+							);
 
-            if (res.ok) {
-              lessons.push({
-                num: res.data['Number'],
-                title: res.data['Title'],
-                link: `plan/lesson${i}`,
-              });
-            }
-          } 
-        }
+							if (res.ok) {
+								lessons.push({
+									num: res.data['Number'],
+									title: res.data['Title'],
+									link: `plan/${sectionKey}/lesson${i}`
+								});
+							}
+						}
+					}
+				}
 
-        return {
-          section: sectionKey,
-          items: lessons
-        }
-      })
-    );
+				const sectionName = details['Name'] || sectionKey;
 
-    return {
+				return {
+					section: sectionName,
+					items: lessons
+				};
+			})
+		);
+
+		return {
 			props: {
-        name: resAbout.data['Name'],
-        desc: resAbout.data['Description'],
+				name: resAbout.data['Name'],
+				desc: resAbout.data['Description'],
 				sections
 			}
 		};
@@ -60,10 +69,10 @@
 </script>
 
 <script>
-  import Plan from '$lib/plan.svelte';
-  
-  export let name;
-  export let sections;
+	import Plan from '$lib/plan.svelte';
+
+	export let name;
+	export let sections;
 </script>
 
-<Plan {name} {sections}/>
+<Plan {name} {sections} />
