4b730a4e995e1c49b164b81212504cd90ebd93ff
zhuiks <zhuke@ukr.net>
2021-06-28 21:09:40 -0500

    add rough annotation box

--- src/lib/lesson.svelte
+++ src/lib/lesson.svelte
@@ -1,12 +1,21 @@
 <script>
 	import 'wired-elements/lib/wired-card.js';
+  import Annotation from 'svelte-rough-notation';
 	// import 'wired-elements/lib/wired-divider.js';
+  import { onMount } from 'svelte';
+	
 	import LessonHeader from './lesson-header.svelte';
 	import MemoryVerse from './memory-verse.svelte';
 
 	export let section;
 	export let lesson;
 
+	let visible = false;
+  onMount(() => {
+    setTimeout(() => {
+      visible = true; 
+    }, 1000);
+  });
 </script>
 
 <article class="lesson">
@@ -30,10 +39,16 @@
 	</section>
 
 	<section class="character">
-		<wired-card elevation="2">
-			<h3>God's character</h3>
-			<p>{lesson['Character']}</p>
-		</wired-card>
+		<div>
+			<Annotation bind:visible type="box" padding={10}>
+				<div class="character-wrapper">
+					<h3>God's character:</h3>
+					{#each lesson['Character'] as item}
+						<div>{item}</div>						
+					{/each}
+				</div>
+			</Annotation>
+		</div>	
 	</section>
 
 	<section>
@@ -65,12 +80,22 @@
     display: flex;
     justify-content: center;
   }
-  .character > wired-card {
+  .character > div {
 		max-width: 80%;
     padding: 1em;
+		color: var(--blue);
+		overflow: hidden;
 	}
-  .character h3 {
-    margin-top: 0.45em;
-    margin-bottom: 0.45em;
+	.character-wrapper {
+		color: var(--text-color);
+		padding: 1em 0;
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		gap: 0.75em 1em;
+	}
+  .character-wrapper h3 {
+		margin: 0;
   }
 </style>
