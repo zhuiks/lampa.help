75c83daca36fb6eebe87e3c7a3598db472330cbc
zhuiks <zhuke@ukr.net>
2021-07-26 15:42:46 -0500

    add projectFluent bundle

--- src/lib/lesson.svelte
+++ src/lib/lesson.svelte
@@ -1,4 +1,28 @@
-<script>	
+<script context="module">
+	import { FluentBundle, FluentResource } from '@fluent/bundle';
+
+	let resource = new FluentResource(`
+main-truth = Main Truth
+summary = Summary
+application = Application
+songs = Song Suggestions
+`);
+
+	let bundle = new FluentBundle('en-US');
+	let errors = bundle.addResource(resource);
+	if (errors.length) {
+		console.log(errors);
+	}
+
+	function getMessage(id) {
+		const message = bundle.getMessage(id);
+		if (message && message.value) {
+			return bundle.formatPattern(message.value);
+		}
+		return id;
+	}
+</script>
+<script>
 	import LessonHeader from './elements/lesson-header.svelte';
 	import MemoryVerse from './elements/memory-verse.svelte';
 	import Character from './elements/character.svelte';
@@ -17,20 +41,20 @@
 
 	<MemoryVerse verse={lesson['Verse']} />
 
-	<h3 class="truth">Main Truth</h3>
+	<h3 class="truth">{getMessage("main-truth")}</h3>
 	<h2>{lesson['Truth']}</h2>
 
 	<section>
-		<h3>Summary</h3>
+		<h3>{getMessage("summary")}</h3>
 		{#each lesson['Summary'].split('\n') as paragraph}
 			<p>{paragraph}</p>
 		{/each}
 	</section>
 
-	<Character values={lesson['Character']}/>
+	<Character values={lesson['Character']} />
 
 	<section>
-		<h3>Application</h3>
+		<h3>{getMessage("application")}</h3>
 		{#each lesson['Application'].split('\n') as paragraph}
 			<p>{paragraph}</p>
 		{/each}
@@ -38,12 +62,12 @@
 
 	<footer>
 		{#if lesson['Songs']}
-		<h3>Song suggestions</h3>
-		<ul>
-			{#each lesson['Songs'] as song}
-				<li>{song}</li>
-			{/each}
-		</ul>
+			<h3>{getMessage("songs")}</h3>
+			<ul>
+				{#each lesson['Songs'] as song}
+					<li>{song}</li>
+				{/each}
+			</ul>
 		{/if}
 	</footer>
 </article>
@@ -65,6 +89,6 @@
 		text-align: center;
 		font-weight: normal;
 		color: #999;
-		width: 180px
+		width: 180px;
 	}
 </style>
