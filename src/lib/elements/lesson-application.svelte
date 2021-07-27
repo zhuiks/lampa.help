051601c51ae10e8d43b5a6bb2b7d6a9c8408aaec
zhuiks <zhuke@ukr.net>
2021-07-27 07:33:11 -0500

    prayer icon

--- src/lib/elements/lesson-application.svelte
+++ src/lib/elements/lesson-application.svelte
@@ -1,26 +1,31 @@
 <script>
 	export let text = '';
 
-	const paragraphs = text.split('\n').reduce((a, p) => {
-    var arr = [...a];
-    const found = p.match(/^(.+)__(.+)__$/);
-    if(found) {
-      arr.push({text: found[1]}, {prayer: true, text: found[2]});
-    } else {
-      arr.push({text: p});
-    }
-    return arr;
-  }, []);
+	const paragraphs = text.split('\n').map((p) => {
+		const found = p.match(/^(.+)__(.+)__$/);
+		if (found) {
+			return { text: found[1], prayer: found[2] };
+		}
+		return { text: p };
+	}, []);
 </script>
 
-{#each paragraphs as {text, prayer}}
-	<p class:prayer>{text}</p>
+{#each paragraphs as { text, prayer }}
+	<p>
+		{text}
+		{#if prayer}
+			<em class="prayer">{prayer}</em>
+		{/if}
+	</p>
 {/each}
 
 <style>
-  .prayer {
-    margin-top: -0.5em;
-    padding-inline-start: 3em;
-    font-style: italic;
-  }
+	.prayer {
+		font-style: italic;
+	}
+	.prayer::before {
+		content: '🙏';
+    font-style: normal;
+    padding-inline-start: 0.5em;
+	}
 </style>
