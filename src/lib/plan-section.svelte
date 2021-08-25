7c3e7fbc18852d4cf8dc4d49098c5e7e1f3ea067
zhuiks <zhuke@ukr.net>
2021-08-25 17:47:58 -0500

    align plan section header

--- src/lib/plan-section.svelte
+++ src/lib/plan-section.svelte
@@ -1,5 +1,5 @@
 <script>
-  import PlanItem from './plan-item.svelte';
+	import PlanItem from './plan-item.svelte';
 
 	export let expanded = true;
 	export let section;
@@ -11,37 +11,59 @@
 </script>
 
 <section>
-<h3 class:expanded on:click={toggle}>{section}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="arrow"><path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z"/></svg></h3>
+	<h3 class:expanded on:click={toggle}>
+		{section}
+		<svg class="arrow" width="16" height="26" viewBox="0 0 16 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M9 2C8.21885 8.93041 7 16.0426 7 23"
+				stroke-width="3"
+				stroke-linecap="round"
+			/>
+			<path
+				d="M2 15C3.33102 17.6165 5.38346 27.9117 9.41333 22.4016C10.2097 21.3128 10.6185 19.8331 11.4133 18.8001C12.1088 17.8961 13.6288 17.3208 14 16.5314"
+				stroke-width="3"
+				stroke-linecap="round"
+			/>
+		</svg>
+	</h3>
 
-{#if expanded}
-	<nav>
-		{#each items as item}
-			<PlanItem {...item} />
-		{/each}
-	</nav>
-{/if}
+	{#if expanded}
+		<nav>
+			{#each items as item}
+				<PlanItem {...item} />
+			{/each}
+		</nav>
+	{/if}
 </section>
 
 <style>
-  section {
-    margin-top: 1.5em;
-  }
-  h3 {
+	section {
+		margin-top: 1.5em;
+	}
+	h3 {
 		/* padding: 0 0 0 1.5em; */
 		cursor: pointer;
-    display: flex;
-    align-items: center;
-    margin: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 0;
+		text-decoration: underline;
 	}
 
-  .arrow {
-    width: 1.25em;
-    height: 1.25em;
-    padding-bottom: 0.3em;
-    fill: var(--orange);
-  }
+	h3.expanded {
+		text-decoration: none;
+	}
 
-	.expanded .arrow{
-		transform: rotate(90deg);
+	.arrow {
+		height: 0.8em;
+		padding-left: 0.3em;
+		stroke: darkgray;
+		transform: scale(0);
+	}
+	h3:hover .arrow {
+		transform: scale(1);
+	}
+	.expanded:hover .arrow {
+		transform: rotate(170deg);
 	}
 </style>
