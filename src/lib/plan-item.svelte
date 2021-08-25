1db0b71cfe77df3fca800f96de0299b48583d452
zhuiks <zhuke@ukr.net>
2021-08-25 16:51:20 -0500

    change lesson plan layout

--- src/lib/plan-item.svelte
+++ src/lib/plan-item.svelte
@@ -1,22 +1,65 @@
 <script>
-  import WiredLink from './wired/wired-link.svelte';
-
-  export let num = '';
-  export let title;
-  export let passage='';
-  export let link;
+	export let num = '';
+	export let title;
+	export let passage = '';
+	export let link;
 </script>
 
-{#if num}
-  <strong>#{num}</strong> 
-{/if}
+<div class="container">
+	<div class="line" />
+	{#if num}
+		<a class="circle" href={link}><strong>#{num}</strong></a>
+	{/if}
 
-<WiredLink href={link}>{title}</WiredLink>
-
-<span class="passage">{passage.split(' (')[0]}</span>
+	<div class="title">
+		<a href={link}>{title}</a>
+		<div class="details">{passage.split(' (')[0]}</div>
+	</div>
+</div>
 
 <style>
-  .passage {
-    color: #999;
-  }
+	.container {
+		--circle-size: 2em;
+		width: 100%;
+		display: grid;
+		grid-auto-flow: column;
+		grid-template-columns: 1fr calc(var(--circle-size) * 1.3) 1fr;
+		min-height: calc(var(--circle-size) * 1.3);
+		align-items: center;
+	}
+	.line {
+		grid-column: 2;
+    grid-row: 1;
+		width: calc(50% - 1px);
+		border-inline-end: 3px solid var(--heading-color);
+    align-self: stretch;
+	}
+	.circle {
+    grid-column: 2;
+    grid-row: 1;
+    place-self: center;
+		background: white;
+		display: block;
+		height: var(--circle-size);
+		width: var(--circle-size);
+		line-height: var(--circle-size);
+		text-align: center;
+		font-size: 0.6em;
+		letter-spacing: -1px;
+		border-radius: 50%;
+		border: 2px solid var(--heading-color);
+	}
+
+	.title {
+		text-align: right;
+		word-break: break-word;
+	}
+	.container:nth-child(even) .title {
+		grid-column: 3;
+		text-align: left;
+	}
+	.details {
+		color: darkgrey;
+		font-size: 0.6em;
+	}
 </style>
--- src/lib/plan-section.svelte
+++ src/lib/plan-section.svelte
@@ -14,13 +14,11 @@
 <h3 class:expanded on:click={toggle}>{section}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="arrow"><path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z"/></svg></h3>
 
 {#if expanded}
-	<ul>
+	<nav>
 		{#each items as item}
-			<li>
-				<PlanItem {...item} />
-			</li>
+			<PlanItem {...item} />
 		{/each}
-	</ul>
+	</nav>
 {/if}
 </section>
 
@@ -45,19 +43,5 @@
 
 	.expanded .arrow{
 		transform: rotate(90deg);
-	}
-
-	ul {
-    margin: 0;
-    padding: 0;
-		list-style: none;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-    grid-auto-flow: row;
-    grid-gap: 0.5em 1.5em;
-	}
-
-	li {
-		padding: 0;
 	}
 </style>
