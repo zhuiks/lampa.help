581e5601852d248420610b0a944323ca18e62f8b
zhuiks <zhuke@ukr.net>
2021-08-27 14:57:11 -0500

    add adapter-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import svelteFluent from '@nubolab-ffwd/svelte-fluent/rollup-plugin';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: netlify(),
 		prerender: {
 			enabled: false
 		},
