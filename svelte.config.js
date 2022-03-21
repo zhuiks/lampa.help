d8d793b341b92edbbd5e695d1fcb9f2dd465196f
zhuiks <zhuke@ukr.net>
2022-03-20 19:17:50 -0500

    rebuild with fallback

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import svelteFluent from '@nubolab-ffwd/svelte-fluent/rollup-plugin';
 import adapter from "@sveltejs/adapter-static";
 
-const dev = process.env.NODE_ENV === "development";
+// const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,13 +9,14 @@ const config = {
 		adapter: adapter({
 			// fallback: "docs/index.html",
 			pages: "docs",
-			assets: "docs"
+			assets: "docs",
+			fallback: "index.html"
 		}),
-		paths: {
-			base: dev ? "" : "/little-lamp",
-		},
+		// paths: {
+		// 	base: dev ? "" : "/little-lamp",
+		// },
 		// prerender: {
-		// 	enabled: false
+		// 	default: true
 		// },
 		// ssr: false,
 		// hydrate the <div id="svelte"> element in src/app.html
